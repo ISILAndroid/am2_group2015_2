c385c0cd7168a298c35c55fc310ef8167dd75f0c
emedinaa <emedinaa@gmail.com>
2015-08-25 20:31:28 -0500

    example 2

--- AM2Fragments/app/src/main/java/com/isil/am2template/HomeActivity.java
+++ AM2Fragments/app/src/main/java/com/isil/am2template/HomeActivity.java
@@ -1,41 +1,47 @@
 package com.isil.am2template;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.isil.am2template.view.OnFragmentListener;
+import com.isil.am2template.view.fragments.AFragment;
+import com.isil.am2template.view.fragments.BFragment;
 
 
 public class HomeActivity extends ActionBarActivity implements OnFragmentListener {
 
+    private static final String TAG = "HomeActivity";
+
+    private AFragment fragmentA;
+    private BFragment fragmentB;
+    private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
+    }
+
+    private void init() {
+        fragmentManager= getSupportFragmentManager();
+        fragmentA= (AFragment)fragmentManager.findFragmentById(R.id.fragmentA);
+        fragmentB= (BFragment)fragmentManager.findFragmentById(R.id.fragmentB);
+
+        Log.v(TAG, "fragment A"+fragmentA+ " fragment B "+fragmentB);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
+        return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
     @Override
