705514942a0923658ecdfb4088cee059b38a39c0
Eduardo Medina <em@osp.pe>
2015-09-01 13:24:15 -0500

    base ejemplo

---
+++ ExamplesFragments/app/src/main/java/com/isil/am2template/view/OnColorListener.java
@@ -0,0 +1,10 @@
+package com.isil.am2template.view;
+
+/**
+ * Created by emedinaa on 1/09/15.
+ */
+public interface OnColorListener {
+
+    void selectedColor(int color, int position);
+    void selectedColor(int color);
+}
