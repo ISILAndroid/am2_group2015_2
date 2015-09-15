20ff93e8de908523eada154c2f29cf97db4a62a9
Eduardo Medina <em@osp.pe>
2015-09-15 18:16:22 -0500

    fragments

--- MyNotes/app/src/main/java/com/isil/mynotes/LoginActivity.java
+++ MyNotes/app/src/main/java/com/isil/mynotes/LoginActivity.java
@@ -1,38 +1,87 @@
 package com.isil.mynotes;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.isil.mynotes.storage.PreferencesHelper;
 
 
 public class LoginActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+    private Button btnLogin;
+    private EditText eteUsername;
+    private EditText etePassword;
+    private String username;
+    private String password;
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+    }
+
+    private void init() {
+        eteUsername=(EditText)findViewById(R.id.eteUsername);
+        etePassword=(EditText)findViewById(R.id.etePassword);
+        btnLogin=(Button)findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateForm()) {
+                    gotoMain();
+                }
+            }
+        });
+    }
+
+    private void gotoMain() {
+
+        savePreferences();
+        Intent intent= new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void savePreferences() {
+
+        PreferencesHelper.saveSession(this,username,password);
+    }
+
+    private boolean validateForm() {
+        username= eteUsername.getText().toString();
+        password= etePassword.getText().toString();
+
+        if(username.isEmpty())
+        {
+            eteUsername.setError("Error campo username");
+            return false;
+        }
+        if(password.isEmpty())
+        {
+            etePassword.setError("Error campo password");
+            return false;
+        }
         return true;
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return false;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 }
--- MyNotes/app/src/main/java/com/isil/mynotes/MainActivity.java
+++ MyNotes/app/src/main/java/com/isil/mynotes/MainActivity.java
@@ -1,38 +1,118 @@
 package com.isil.mynotes;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.isil.mynotes.model.entity.NoteEntity;
+import com.isil.mynotes.storage.PreferencesHelper;
+import com.isil.mynotes.storage.db.CRUDOperations;
+import com.isil.mynotes.storage.db.MyDatabase;
+import com.isil.mynotes.utils.StringUtils;
+import com.isil.mynotes.view.adapters.NoteAdapter;
+
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG ="MainActivity" ;
+
+    private TextView tviLogout,tviUser;
+    private ListView lstNotes;
+    private Button btnAddNote;
+    private List<NoteEntity> lsNoteEntities;
+    private CRUDOperations crudOperations;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //populate();
+        init();
+        loadData();
+    }
+
+    private void loadData() {
+        crudOperations= new CRUDOperations(new MyDatabase(this));
+        lsNoteEntities= crudOperations.getAllNotes();
+        noteAdapter= new NoteAdapter(this,lsNoteEntities);
+        lstNotes.setAdapter(noteAdapter);
+
+    }
+
+    private void populate() {
+
+        CRUDOperations crudOperations= new CRUDOperations(new MyDatabase(this));
+        crudOperations.addNote(new NoteEntity("Mi Nota","Esta es un nota ",null));
+        crudOperations.addNote(new NoteEntity("Segunda Nota","Esta es la segunds nota ",null));
+        crudOperations.addNote(new NoteEntity("Tercera Nota","Esta es la tercera nota ",null));
+        crudOperations.addNote(new NoteEntity("Cuarta Nota","Esta es la cuarta nota ",null));
+        crudOperations.addNote(new NoteEntity("Quinta Nota","Esta es la quinta nota ",null));
+        crudOperations.addNote(new NoteEntity("Sexta Nota","Esta es la sexta nota ",null));
+
+        Log.v(TAG, "populate "+crudOperations.getAllNotes());
+    }
+
+    private void init() {
+        tviLogout= (TextView)findViewById(R.id.tviLogout);
+        tviUser= (TextView)findViewById(R.id.tviUser);
+        lstNotes= (ListView)(findViewById(R.id.lstNotes));
+        btnAddNote= (Button)(findViewById(R.id.btnAddNote));
+
+        //user Info
+        String username = PreferencesHelper.getUserSession(this);
+        if(username!=null)
+        {
+            tviUser.setText("Bienvenido "+ StringUtils.firstCapitalize(username));
+        }
+
+        //events
+        btnAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        lstNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+        tviLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        PreferencesHelper.signOut(this);
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+        return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 }
