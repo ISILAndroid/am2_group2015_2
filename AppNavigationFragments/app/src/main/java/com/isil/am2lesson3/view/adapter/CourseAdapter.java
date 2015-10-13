f9f44686a6580e7733728c2d746c32ffe0e2f7b5
emedinaa <emedinaa@gmail.com>
2015-10-12 21:40:10 -0500

    patrón holder

--- AppNavigationFragments/app/src/main/java/com/isil/am2lesson3/view/adapter/CourseAdapter.java
+++ AppNavigationFragments/app/src/main/java/com/isil/am2lesson3/view/adapter/CourseAdapter.java
@@ -41,25 +41,48 @@ public class CourseAdapter extends BaseAdapter {
         return 0;
     }
 
+    static class ViewHolder {
+        TextView tviName;
+        TextView tviGrade;
+        ImageView imgGrade;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater=LayoutInflater.from(context);
-        View container= inflater.inflate(R.layout.row_course, null);
-        ImageView imgContact= (ImageView)container.findViewById(R.id.iviCourse);
-        TextView tviName= (TextView)container.findViewById(R.id.tviName);
-        TextView tviGrade= (TextView)container.findViewById(R.id.tviGrade);
+        ViewHolder viewHolder;
+        if(convertView==null){
+
+            // inflate the layout
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView= inflater.inflate(R.layout.row_course, null);
+
+            // well set up the ViewHolder
+            viewHolder = new ViewHolder();
+            viewHolder.imgGrade= (ImageView)convertView.findViewById(R.id.iviCourse);
+            viewHolder.tviName= (TextView)convertView.findViewById(R.id.tviName);
+            viewHolder.tviGrade= (TextView)convertView.findViewById(R.id.tviGrade);
+
+            // store the holder with the view.
+            convertView.setTag(viewHolder);
+
+        }else{
+            // we've just avoided calling findViewById() on resource everytime
+            // just use the viewHolder
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
         //Extraer la entidad
         CourseEntity courseEntity= this.data.get(position);
+        if(courseEntity!=null)
+        {
+            //Asociar la entidad con el XML
+            String name = courseEntity.getName();
+            double grade = courseEntity.getGrade();
+            viewHolder.tviName.setText(name);
+            viewHolder.tviGrade.setText(Double.toString(grade));
+            //imgContact.setImageResource(contactEntity.getPhoto());
+        }
 
-        //Asociar la entidad con el XML
-        String name = courseEntity.getName();
-        double grade = courseEntity.getGrade();
-        tviName.setText(name);
-        tviGrade.setText(Double.toString(grade));
-
-        //imgContact.setImageResource(contactEntity.getPhoto());
-
-        return container;
+        return convertView;
     }
 }
